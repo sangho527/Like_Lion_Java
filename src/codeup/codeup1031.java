65d00d3a0ebe24155644c6de71401872fa4e4834
sangho527 <sy980527@naver.com>
2022-10-09 01:17:00 +0900

    codeup 문제풀이 221009

---
+++ src/codeup/codeup1031.java
@@ -0,0 +1,13 @@
+
+package codeup;
+import java.util.Scanner;
+
+public class codeup1031 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int value = sc.nextInt();
+
+        System.out.println(String.format("%o", value));
+    }
+}
