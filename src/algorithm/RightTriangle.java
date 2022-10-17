557379f4242467d64025ed217a59ac26f3f11904
sangho527 <sy980527@naver.com>
2022-10-17 09:35:24 +0900

    별찍기 221017

--- src/algorithm/RightTriangle.java
+++ src/algorithm/RightTriangle.java
@@ -1,10 +1,25 @@
 package algorithm;
 
 public class RightTriangle {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         System.out.println("*");
         System.out.println("**");
         System.out.println("***");
         System.out.println("****");
+    }*/
+
+    public void PrintStar(int n){
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j <=i; j++){
+                System.out.printf("*");
+            }
+            System.out.printf("\n");
+        }
     }
+
+    public static void main(String[] args) {
+        RightTriangle rt = new RightTriangle();
+        rt.PrintStar(10);
+    }
+
 }
