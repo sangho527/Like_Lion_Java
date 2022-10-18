025bc7a27bb0ca7d82b1a02af779175fbebdfa92
sangho527 <sy980527@naver.com>
2022-10-18 09:19:04 +0900

    정사각형 별찍기 221018

--- src/algorithm/SquareStar.java
+++ src/algorithm/SquareStar.java
@@ -1,9 +1,20 @@
 package algorithm;
 
 public class SquareStar {
+
+    public void printSqure(int n){
+        for (int i = 0; i < n; i++){
+            System.out.println("***");
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("***");
-        System.out.println("***");
-        System.out.println("***");
+//        System.out.println("***");
+//        System.out.println("***");
+//        System.out.println("***");
+        SquareStar SS = new SquareStar();
+        SS.printSqure(3);
     }
 }
