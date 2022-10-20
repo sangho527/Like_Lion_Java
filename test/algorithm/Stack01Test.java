8e62ec0cbbe981e5c1e96dc68c4f93a2ca8e542e
sangho527 <sy980527@naver.com>
2022-10-20 10:30:24 +0900

    스택 isEmpty, push, pop 221020

--- test/algorithm/Stack01Test.java
+++ test/algorithm/Stack01Test.java
@@ -1,5 +1,6 @@
 package algorithm;
 
+import algorithm.stack.Stack01;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,23 +8,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Stack01Test {
-
+    Stack01 st = new Stack01();
 
     @BeforeEach
     void setUp() {
+        st.push(10);
+        st.push(10);
     }
-
 
     @Test
     @DisplayName("push가 잘 되는지")
     void push(){
-        Stack01 st = new Stack01();
-        st.push(10);
-        st.push(20);
         Integer[] arr = st.getArr();
         assertEquals(20,arr[1]);
         assertEquals(10,arr[0]);
+    }
 
+    @Test
+    void isEmpty() {
+        Stack01 st = new Stack01();
+        assertTrue(st.isEmpty());
+        st.push(10);
+        assertTrue(st.isEmpty());
+        st.pop();
+        assertTrue(st.isEmpty());
     }
     @Test
     void pushAndPop(){
@@ -33,6 +41,7 @@ class Stack01Test {
 
         assertEquals(20, st.pop());
         assertEquals(10, st.pop());
+        st.pop();
 
     }
 }
