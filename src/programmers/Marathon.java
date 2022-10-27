9d66bdd8a546f95daa0efc42636d3f00ed6d40e1
sangho527 <sy980527@naver.com>
2022-10-27 10:50:57 +0900

    Programmers 221027

--- src/programmers/Marathon.java
+++ src/programmers/Marathon.java
@@ -1,35 +1,33 @@
 package programmers;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
-public class Marathon { // completion 의 길이는 participant의 길이보다 1작아야함
+public class Marathon {
 
     public String Marathon(String[] participant, String[] completion) {
+
         String answer = "";
 
-        HashMap<String, Integer> map = new HashMap<>();
+        ArrayList<String> participantList = new ArrayList<String>();
 
-        for (String name : participant)
+        for (String name : participant) {
 
-        {
-            map.put(name, map.getOrDefault(name, 0) + 1);
+            participantList.add(name);
         }
 
-        for (String name : completion)
-        {
-            map.put(name, map.get(name) - 1);
-        }
+        for (String name : completion) {
 
-        for (String key : map.keySet())
-        {
-            if(map.get(key) > 0)
-            {
-                answer = key;
-                break;
+            for (int i = 0; i < participantList.size(); i++) {
+
+                if (participantList.get(i).equals(name)) {
+
+                    participantList.remove(i);
+
+                    break;
+                }
             }
         }
-
+        answer = participantList.get(0);
         return answer;
-    };
-
+    }
 }
