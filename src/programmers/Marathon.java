41c1b0613e40ed22194ca8fe0356045184f849c3
sangho527 <sy980527@naver.com>
2022-10-27 09:28:54 +0900

    Programmers 221027

---
+++ src/programmers/Marathon.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class Marathon { // completion 의 길이는 participant의 길이보다 1작아야함
+
+    public String Marathon(String[] participant, String[] completion) {
+        String answer = "";
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for
+
+        return answer;
+    }
+
+}
