88ffb10a3cdbb238fa31ea6182f3578e6778451d
sangho527 <sy980527@naver.com>
2022-10-31 11:08:23 +0900

    프로그래머스 모의고사 완료 221031

--- src/programmers/MockExam.java
+++ src/programmers/MockExam.java
@@ -1,5 +1,7 @@
 package programmers;
 
+import java.util.ArrayList;
+
 public class MockExam {
     public int[] solution(int[] answers) {
 
@@ -13,19 +15,22 @@ public class MockExam {
 
         for (int i = 0; i < answers.length; i++) {
 
-            if (answers[i] == student1[ i % 5]) answer1 ++; // 1번
-            if (answers[i] == student2[ i % student2.length]) answer2 ++; // 2번
-            if (answers[i] == student3[ i % 10]) answer3 ++; // 3번
+            if (answers[i] == student1[i % 5]) answer1++; // 1번
+            if (answers[i] == student2[i % student2.length]) answer2++; // 2번
+            if (answers[i] == student3[i % 10]) answer3++; // 3번
 
         }
+        int max = Math.max(Math.max(answer1, answer2), answer3); // max값 구하기
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (max == answer1) list.add(1);
+        if (max == answer2) list.add(2);
+        if (max == answer3) list.add(3);
 
+        answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-
-
-
     }
 }
