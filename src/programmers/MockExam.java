c48a6f2c3e950915123fa1bbb3494374791b2c96
sangho527 <sy980527@naver.com>
2022-10-31 09:35:00 +0900

    프로그래머스 모의고사 설계중221031

---
+++ src/programmers/MockExam.java
@@ -0,0 +1,24 @@
+package programmers;
+
+public class MockExam {
+    public int[] solution(int[] answers){
+
+        int[] answer = {};
+        int[] student1 = {1,2,3,4,5};
+        int[] student2 = {2,1,2,3,2,4,2,5};
+        int[] student3 = {3,3,1,1,2,2,4,4,5,5};
+
+        }
+        // 12345 패턴으로 찍는 학생이 몇 문제를 맞췄는지 알아내는 코드
+
+
+
+        return new int[];
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
