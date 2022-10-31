2252d0aa85f5c37b3b718b1c551c29b7aa26cbdb
sangho527 <sy980527@naver.com>
2022-10-31 10:36:06 +0900

    프로그래머스 모의고사 for문작성 221031

--- src/programmers/MockExam.java
+++ src/programmers/MockExam.java
@@ -1,19 +1,26 @@
 package programmers;
 
 public class MockExam {
-    public int[] solution(int[] answers){
+    public int[] solution(int[] answers) {
 
         int[] answer = {};
-        int[] student1 = {1,2,3,4,5};
-        int[] student2 = {2,1,2,3,2,4,2,5};
-        int[] student3 = {3,3,1,1,2,2,4,4,5,5};
+        int[] student1 = {1, 2, 3, 4, 5};
+        int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int answer1 = 0;
+        int answer2 = 0;
+        int answer3 = 0;
+
+        for (int i = 0; i < answers.length; i++) {
+
+            if (answers[i] == student1[ i % 5]) answer1 ++; // 1번
+            if (answers[i] == student2[ i % student2.length]) answer2 ++; // 2번
+            if (answers[i] == student3[ i % 10]) answer3 ++; // 3번
 
         }
-        // 12345 패턴으로 찍는 학생이 몇 문제를 맞췄는지 알아내는 코드
 
 
-
-        return new int[];
+        return answer;
     }
 
     public static void main(String[] args) {
