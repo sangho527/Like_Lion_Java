3ea6f122fab13f11d3e0462bbde3e49fc133fdcb
sangho527 <sy980527@naver.com>
2022-11-03 09:30:47 +0900

    소수판별 알고리즘 진행중 221103

---
+++ src/programmers/PrimeNumber2.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class PrimeNumber2 {
+    //2의 배수 지울때 2, 4, 6 순서
+    //3의 배수 지울 때 4, 7, 10 …
+    //4의 배수 지울 때 6, 10, 14… (4의 배수 꼭 지워보기)
+    //5의 배수 지울 때 8, 13, 18 ...
+    public static void main(String[] args) {
+
+    }
+}
