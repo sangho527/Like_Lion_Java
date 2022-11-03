e10c7463539b850794f1352819091eeb8f3a5ce0
sangho527 <sy980527@naver.com>
2022-11-03 10:16:07 +0900

    소수판별 알고리즘 기본셋팅 221103

--- src/programmers/PrimeNumber2.java
+++ src/programmers/PrimeNumber2.java
@@ -1,11 +1,19 @@
 package programmers;
 
+import java.util.Arrays;
+
 public class PrimeNumber2 {
     //2의 배수 지울때 2, 4, 6 순서
     //3의 배수 지울 때 4, 7, 10 …
     //4의 배수 지울 때 6, 10, 14… (4의 배수 꼭 지워보기)
     //5의 배수 지울 때 8, 13, 18 ...
     public static void main(String[] args) {
+
+        int N = 50;
+        int[] nums = new int[N-1];
+        boolean[] checks = new boolean[nums.length];
+        Arrays.fill(checks, true); //
+        for (int i = 0; i < nums.length; i++) nums[i] = i + 2;
 
     }
 }
