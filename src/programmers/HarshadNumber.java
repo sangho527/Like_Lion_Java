6a28f4231362b5905ef12aa05450dd6511026831
sangho527 <sy980527@naver.com>
2022-11-07 09:38:10 +0900

    하샤드 수 221107

--- src/programmers/HarshadNumber.java
+++ src/programmers/HarshadNumber.java
@@ -1,4 +1,19 @@
 package programmers;
 
 public class HarshadNumber { // 하샤드 수
+    public boolean solution(int x) {
+        boolean answer = true;
+        int num = x;
+        int sum = 0;
+
+        while (num != 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+
+        if(x % sum != 0){
+            answer = false;
+        }
+        return answer;
+    }
 }
