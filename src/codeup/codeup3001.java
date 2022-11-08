97b99a0d1892ad695b0ba61fe05c7cef5d51cf00
sangho527 <sy980527@naver.com>
2022-11-08 09:39:46 +0900

    단순탐색 진행중 221108

---
+++ src/codeup/codeup3001.java
@@ -0,0 +1,18 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup3001 { // 단순 탐색
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int n = sc.nextInt(); // --> 첫번째 숫자 10
+        int[] numbers = new int[n];
+        fori  i < n //→ loop 10회
+        numbers[i] = sc.nextInt();
+    }
+}
