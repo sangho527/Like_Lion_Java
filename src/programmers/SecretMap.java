cbe243df56d4bdef8f51d8ddc7fd1ead88fe30cb
sangho527 <sy980527@naver.com>
2022-11-11 09:40:07 +0900

    비밀 지도 풀이방법 221111

--- src/programmers/SecretMap.java
+++ src/programmers/SecretMap.java
@@ -9,7 +9,7 @@ public class SecretMap {
         public String[] solution(int n, int[] arr1, int[] arr2) {
             String[] answer = new String[n];
             for (int i = 0; i < n; i++) {
-
+                answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
             }
             return answer;
         }
