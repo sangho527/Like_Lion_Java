c47765aaa84f961cd7f9fae45d9fdb2700a61b1b
sangho527 <sy980527@naver.com>
2022-11-11 09:38:39 +0900

    비밀 지도 풀이방법 22111

---
+++ src/programmers/SecretMap.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class SecretMap {
+
+    // 한변의 길이가 n, 칸은 # or 공백
+    // 2진수로 풀이 #은 1, 공백은 0
+    // 배열의 수를 2진수로 변경, 비트연산 OR 사용
+    // Integer.toBinaryString() 함수로 정수 -> 2진수 변환
+        public String[] solution(int n, int[] arr1, int[] arr2) {
+            String[] answer = new String[n];
+            for (int i = 0; i < n; i++) {
+
+            }
+            return answer;
+        }
+    }
+
