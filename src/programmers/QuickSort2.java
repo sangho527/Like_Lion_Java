1c77d86651af6bd1e07363ee3b6948e3da63244a
sangho527 <sy980527@naver.com>
2022-11-16 09:32:16 +0900

    [ADD] QuickSort 221116

---
+++ src/programmers/QuickSort2.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class QuickSort2 {
+
+    public static void main(String[] args) { // 피벗 구하기
+        var arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        int pivot = arr[arr.length / 2];
+        System.out.println(pivot);
+    }
+
+
+}
