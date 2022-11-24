5c262e4ef98acf39c730e4b1ba8c0970ddd3e7c6
sangho527 <sy980527@naver.com>
2022-11-24 09:41:25 +0900

    [UPDATE] 피보나치수열 조건 추가 221124

--- src/programmers/Fibonacci.java
+++ src/programmers/Fibonacci.java
@@ -2,6 +2,7 @@ package programmers;
 
 public class Fibonacci {
     public static int fib(int n) {
+        if(n == 1 | n == 2 ) return 1;
         return fib(n - 1) + fib(n - 2);
     }
     public static void main(String[] args) {
