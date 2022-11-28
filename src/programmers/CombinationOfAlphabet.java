6f70f2ada2d0d788cd7ca01c4cba48453e6e27bb
sangho527 <sy980527@naver.com>
2022-11-28 09:20:13 +0900

    [ADD] 알파벳 분할 출력 221128

---
+++ src/programmers/CombinationOfAlphabet.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class CombinationOfAlphabet {
+    private static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 26; i++) {
+            System.out.println(alphabet.charAt(i));
+        }
+    }
+}
