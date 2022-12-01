80933af85a077338fbf9e82a2313ddf47d8a243a
sangho527 <sy980527@naver.com>
2022-12-01 23:47:26 +0900

    [ADD] 221130 부모 노드 찾기

---
+++ src/programmers/FindParent.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class FindParent {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        // a의 부모는
+        int parentA = Math.floorDiv(3, 2);
+
+        System.out.printf("parentA: %d", parentA);
+    }
+
+}
